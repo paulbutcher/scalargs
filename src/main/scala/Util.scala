46ff4faa92b4539f8b2ddcc70edc78dcb322a40c
Paul Butcher <paul@paulbutcher.com>
2010-04-24 02:45:22 +0100

    Minor tidyup

--- src/main/scala/Util.scala
+++ src/main/scala/Util.scala
@@ -2,7 +2,7 @@ package com.paulbutcher.scalargs
 
 object Util {
   def asPairs[T](seq: Seq[T]) = seq match {
-    case s if s.isEmpty => List()
+    case s if s.isEmpty => Seq()
     case s => {
       val successors = s.tail.map(x => Some(x)) :+ None
       s zip successors
