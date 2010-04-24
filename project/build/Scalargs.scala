1e189dd527159b6246bd00a6d327a8f2873dd15e
Paul Butcher <paul@paulbutcher.com>
2010-04-24 23:30:57 +0100

    Remove unnecessary test

--- project/build/Scalargs.scala
+++ project/build/Scalargs.scala
@@ -2,5 +2,5 @@ import sbt._
 
 class Scalargs(info: ProjectInfo) extends DefaultProject(info) { 
   val scalaToolsSnapshots = "Scala-Tools Snapshots" at "http://scala-tools.org/repo-snapshots"
-  val scalatest = "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.RC1-SNAPSHOT" % "test"
+  val scalatest = "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.RC1-SNAPSHOT"
 }
